3221492e762833d9d3a87652df21583d4e5e6b1f
Matthias Schroeder <matthias.schroeder@uni-hamburg.de>
2026-02-22 18:04:56 +0100

    Run 3 AZH fragment

---
+++ fragments/fragment_Run3Summer24wmLHEGS_AZH.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13600.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            'TauDecays:externalMode = 2'
+        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    )
+    )
+)
+
+
+
+# Link to generator fragment:
+# https://gitlab.cern.ch/cms-b2g/b-2-g-m-csample-requests/-/tree/master/genFragments/Hadronizer/13p6TeV/Hadronizer_TuneCP5_13p6TeV_generic_TauFix_LHE_pythia8_cff.py
